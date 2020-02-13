712bb2b96f7b2bfb06b62c8828693643d05da9ee
Aparna <aparnarenjithpillai@gmail.com>
2020-02-13 17:05:40 -0500

    Aparna Version 5:05 PM

--- PIL.py
+++
@@ -1,2 +0,0 @@
-class ImageTK(object):
-class Image(object):
--- homePage.py
+++ homePage.py
@@ -13,29 +13,45 @@ class Home_page(Frame):
    def create_widgets(self):
        """Create widgets to get story information and to display story."""
        # create instruction label
-       Label(self, text=""
+       Label(self, text="",bg="orange2"
              ).grid(row=0, column=0, sticky=N)
 
-       Label(self, text="Brain Games", font="Courier 40 bold",
-             fg="Navy Blue").grid(row=0, column=1, sticky=N)
+       Label(self, text="Brain Games", font="Courier 40 bold", bg="orange2",
+             fg="firebrick3").grid(row=0, column=1, sticky=N)
 
-       Label(self, text=""
+       Label(self, text="", bg="orange2",
              ).grid(row=0, column=2, sticky=N)
 
        Label(self,
-             text="and", font="Courier 40 bold",
-             fg="Navy Blue").grid(row=1, column=1, sticky=N)
+             text="and", font="Courier 40 bold",bg="orange2",
+             fg="firebrick3").grid(row=1, column=1, sticky=N)
 
        Label(self,
-             text="Puzzles", font="Courier 40 bold",
-             fg="Navy Blue").grid(row=2, column=1, sticky=N)
+             text="Puzzles", font="Courier 40 bold",bg="orange2",
+             fg="firebrick3").grid(row=2, column=1, sticky=N)
 
        Label(self,
-             text="Slogan", font="Courier 25",
-             fg="Navy Blue").grid(row=3, column=1, sticky=N)
+             text="Slogan", font="Courier 20",bg="orange2",
+             fg="firebrick4").grid(row=3, column=1, sticky=N)
 
+       Button(self, text="Word Search",
+              font="Courier 18", bd=5, command=self.wordSearch
+              ).grid(row=4, column=1, sticky=N)
 
+       Button(self, text="Sudoku",
+              font="Courier 18", bd=5, command=self.Sudoku
+              ).grid(row=5, column=1, sticky=N)
 
+       Button(self, text="Crossword",
+              font="Courier 18", bd=5, command=self.cross_word
+              ).grid(row=6, column=1, sticky=N)
+
+   def wordSearch(self):
+       pass
+   def Sudoku(self):
+       pass
+   def cross_word(self):
+       pass
 
 
        # Button(self, text="Play",
