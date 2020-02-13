55139f3eaf5813df1cf91f5cf52aed6230c3cac2
Aparna <aparnarenjithpillai@gmail.com>
2020-02-13 18:09:08 -0500

    Aparna Version 5:05 PM

---
+++ real_word_search.py
@@ -0,0 +1,4 @@
+
+def create_widgets(self):
+    letter_list = ["A" ,"B" ,"C" ,"D" ,"E" ,"F" ,"G" ,"H" ,"I" ,"J" ,"K" ,"L" ,"M" ,"N" ,"O" ,"P" ,"Q" ,"R" ,"S" ,"T"
+                   ,"U" ,"V" ,"W" ,"X" ,"Y" ,"Z"]
--- wordSearch.py
+++ wordSearch.py
@@ -35,6 +35,5 @@ class Word_Search(Frame):
        for line in text_file:
            print(line)
 
-   def create_widgets(self):
-       letter_list = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+
 
