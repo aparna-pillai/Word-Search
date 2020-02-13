782b254424a95bed5c92e119e4ee6a155c8b1018
Aditi <aditi.dam@gmail.com>
2020-02-13 17:56:57 -0500

    word searches

--- wordSearch.py
+++ wordSearch.py
@@ -1,11 +1,12 @@
 from tkinter import *
 
-class Word_Search(Frame):
-   def __init__(self, master):
-       """Initialize Frame."""
-       super(Word_Search, self).__init__(master,background="orange2")
-       self.grid()
-       self.create_widgets()
 
-   def create_widgets(self):
-       pass
+class Word_Search(Frame):
+    def __init__(self, master):
+        """Initialize Frame."""
+        super(Word_Search, self).__init__(master, background="orange2")
+        self.grid()
+        self.create_widgets()
+
+    def create_widgets(self):
+        pass
