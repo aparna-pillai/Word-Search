f6ad1e752601f359214e077ff780346db29eea18
Aditi <aditi.dam@gmail.com>
2020-02-13 20:53:51 -0500

    aaagh

--- real_word_search.py
+++ real_word_search.py
@@ -1,6 +1,7 @@
 from tkinter import *
 import random
 
+
 class Word_Search2(Frame):
     def __init__(self, master, text_file):
         """Initialize Frame."""
@@ -8,8 +9,10 @@ class Word_Search2(Frame):
         super(Word_Search2, self).__init__(master, background="orange2")
         self.grid()
         self.create_widgets()
+
     def create_widgets(self):
-        letter_list = ["A" ,"B" ,"C" ,"D" ,"E" ,"F" ,"G" ,"H" ,"I" ,"J" ,"K" ,"L" ,"M" ,"N" ,"O" ,"P" ,"Q" ,"R" ,"S" ,"T","U" ,"V" ,"W" ,"X" ,"Y" ,"Z"]
+        letter_list = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S",
+                       "T", "U", "V", "W", "X", "Y", "Z"]
         line_list = []
         story = ""
         for line in self.text_file:
@@ -28,20 +31,21 @@ class Word_Search2(Frame):
             for char in line_list:
                 story += char
             line_list = []
-        self.story_txt = Text(self, width = 45, height = 25, wrap = WORD)
-        self.story_txt.grid(row = 0, column = 0, columnspan = 4)
+        self.story_txt = Text(self, width=45, height=25, wrap=WORD)
+        self.story_txt.grid(row=0, column=0, columnspan=4)
         self.story_txt.insert(0.0, story)
 
         Button(self, text="Check", bg="orange2", fg="black",
                font="Courier 10", bd=5, command=self.check
                ).grid(row=5, column=0, sticky=W)
+
     def check(self):
         answer_list = []
         actual_answer_list = []
         answers = self.story_txt.get(0.0, END)
         for thing in answers:
             if thing != " " and thing != "\n":
-                 answer_list.append(thing)
+                answer_list.append(thing)
         print(self.char_per_line)
 
         for line in self.text_file:
