ff93a9d98859a8e5d5768d5a2abd214f0ece3c49
satwika-vemuri <satwika.vemuri@gmail.com>
2020-02-13 19:43:48 -0500

    Satwika 7:43 PM

--- real_word_search.py
+++ real_word_search.py
@@ -11,8 +11,26 @@ class Word_Search2(Frame):
     def create_widgets(self):
         letter_list = ["A" ,"B" ,"C" ,"D" ,"E" ,"F" ,"G" ,"H" ,"I" ,"J" ,"K" ,"L" ,"M" ,"N" ,"O" ,"P" ,"Q" ,"R" ,"S" ,"T","U" ,"V" ,"W" ,"X" ,"Y" ,"Z"]
         line_list = []
+        story = ""
         for line in self.text_file:
             for char in line:
                 if char == ".":
                     random_char = random.choice(letter_list)
+                    line_list.append(random_char)
+                else:
+                    line_list.append(char)
+            for char in line_list:
+                story += char
+            line_list = []
 
+
+        self.story_txt = Text(self, width = 45, height = 25, wrap = WORD)
+        self.story_txt.grid(row = 0, column = 0, columnspan = 4)
+        self.story_txt.insert(0.0, story)
+
+        Button(self, text="Check", bg="orange2", fg="black",
+               font="Courier 10", bd=5, command=self.check
+               ).grid(row=5, column=0, sticky=W)
+
+    def check(self):
+        pass
