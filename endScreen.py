df36dbe35858e4baf7d7adabb1f53b897572ed85
Aparna <aparnarenjithpillai@gmail.com>
2020-02-14 11:04:13 -0500

    Aparna Version 8:51 AM

--- endScreen.py
+++ endScreen.py
@@ -9,23 +9,23 @@ class End_Screen(Frame):
       self.create_widgets()
 
   def create_widgets(self):
-      Label(self, text="We hope you", font="Courier 40 bold", bg="orange2",
+      Label(self, text="We hope you", font="Courier 40 bold",
             fg="firebrick3").grid(row=0, column=1, sticky=N)
 
-      Label(self, text="enjoyed playing", font="Courier 40 bold", bg="orange2",
+      Label(self, text="enjoyed playing", font="Courier 40 bold",
             fg="firebrick3").grid(row=1, column=1, sticky=N)
 
-      Label(self, text="Phoenix Word Search!", font="Courier 45 bold", bg="orange2",
+      Label(self, text="Phoenix Word Search!", font="Courier 45 bold",
             fg="firebrick3").grid(row=2, column=1, sticky=N)
 
-      Label(self, text="", font="Courier 45 bold", bg="orange2",
+      Label(self, text="", font="Courier 45 bold",
             fg="firebrick3").grid(row=3, column=1, sticky=N)
 
       Button(self, text="Exit", bg="tomato", fg="black",
              font="Courier 10 bold", bd=5, command=self.exit
              ).grid(row=4, column=1, sticky=N)
 
-      imageSmall = PhotoImage(file="smallerPhoenix.gif")
+      imageSmall = PhotoImage(file="smallerPhoenixcopy.gif")
       w = Label(self,
                 image=imageSmall
                 ).grid(row=5, column=1,sticky=N)
