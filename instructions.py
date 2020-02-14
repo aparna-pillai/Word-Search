4b4e5f5e501b130cedb91d0bd17992abb73cf997
Aparna <aparnarenjithpillai@gmail.com>
2020-02-14 10:36:43 -0500

    Aparna Version 8:51 AM

--- instructions.py
+++ instructions.py
@@ -2,43 +2,45 @@ from tkinter import *
 
 
 class Instruction_page(Frame):
-  def __init__(self, master, return_home):
-      """Initialize Frame."""
-      self.return_home = return_home
-      super(Instruction_page, self).__init__(master)
-      master.title("Instructions Page")
-      self.grid()
-      self.create_widgets()
+ def __init__(self, master, return_home):
+     """Initialize Frame."""
+     self.return_home = return_home
+     super(Instruction_page, self).__init__(master, background = "mistyrose")
+     master.title("Instructions Page")
+     self.grid()
+     self.create_widgets()
 
-  def create_widgets(self):
-      Label(self, text="").grid(row=0, column=0)
+ def create_widgets(self):
+     Label(self, text="").grid(row=0, column=0)
 
-      self.story_txt = Text(self, font="Verdana 20 bold", fg="Crimson", width=100, height=22, wrap=WORD)
-      self.story_txt.grid(row=0, column=1, columnspan=5)
+     self.story_txt = Text(self, font="Verdana 15 bold", fg="Crimson", bg = "mistyrose", width=60, height=28, wrap=WORD)
+     self.story_txt.grid(row=0, column=1, columnspan=5)
 
-      instructions = ("Welcome to Phoenix Word Search!!!\n\n"
-                      "Here you can challenge your brain by playing word search.\n\n\n"
-                      "For Word Search, you can choose from three levels -- easy, medium, and hard.\n\n"
-                      "When you find a word, replace all the letters of the word in the word search with an "
-                      "\nasterisk [*]. Then press the check button to see if you've found a word!\n\n"
-                      "Bonus! All three word searches have a theme of phoenixes and fire!\n\n"
-                      "If you change a letter that's not part of a word to an asterisk, then when \nthe word search "
-                      "resets, it will change to a different random letter. \n\nDon't get confused!\n\n"
-                      "When you're ready, click the 'home page' button to return to the home screen \nand "
-                      "start your puzzle!\n\n"
-                      "Can you beat the clock and solve them all?"
-                      )
+     instructions = ("Welcome to Phoenix Word Search!!!\n\n"
+                     "Here you can challenge your brain by playing word search.\n\n"
+                     "You can choose from three levels -- easy, medium, and hard.\n\n"
+                     "When you find a word, replace all the letters of the word in the word search with an "
+                     "asterisk [*].\n\nThen press the check button to see if you've found a word!\n\n"
+                     "If time is running out or you are desperate, you can replace a single letter with an asterisk and if that "
+                     "letter is in any of the words, it will turn lowercase.\n\n"
+                     "Bonus! All three word searches have a theme of phoenixes and fire!\n\n"
+                     "If you change a letter that's not part of a word to an asterisk, then when \nthe word search "
+                     "resets, it will change to a different random letter. \n\nDon't get confused!\n\n"
+                     "When you're ready, click the 'home page' button to return to the home screen and "
+                     "start your puzzle!\n\n"
+                     "Can you beat the clock and solve them all?"
+                     )
 
-      self.story_txt.delete(0.0, END)
-      self.story_txt.insert(0.0, instructions)
+     self.story_txt.delete(0.0, END)
+     self.story_txt.insert(0.0, instructions)
 
-      self.home_bttn = Button(self, text="home page", command=self.back_to_home)
+     self.home_bttn = Button(self, text="home page", command=self.back_to_home)
 
-      Button(self, text="Home Page",
-             font="fixedsys 20",              fg="light gray",
-             bg="maroon",
-             command=self.back_to_home
-             ).grid(row=4, column=1, sticky=W)
+     Button(self, text="Home Page",
+            font="fixedsys 20",              fg="light gray",
+            bg="maroon",
+            command=self.back_to_home
+            ).grid(row=4, column=1, sticky=W)
 
-  def back_to_home(self):
-      self.return_home()
+ def back_to_home(self):
+     self.return_home()
--- real_word_search.py
+++ real_word_search.py
@@ -1,108 +1,115 @@
 from tkinter import *
 import random
-from Timer import ExampleApp
 
 
 class Word_Search2(Frame):
- def __init__(self, master, text_file):
-     """Initialize Frame."""
-     self.text_file = text_file
-     super(Word_Search2, self).__init__(master, background="coral")
-     self.grid()
-     self.create_widgets()
+   def __init__(self, master, text_file, end_screen):
+       """Initialize Frame."""
+       self.end_screen = end_screen
+       self.text_file = text_file
+       super(Word_Search2, self).__init__(master, background="coral")
+       self.grid()
+       self.create_widgets()
 
- def create_widgets(self):
+   def create_widgets(self):
 
-     self.letter_list = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R",
-                         "S",
-                         "T", "U", "V", "W", "X", "Y", "Z"]
-     letter_list = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S",
-                    "T", "U", "V", "W", "X", "Y", "Z"]
-     lowerr_list = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s",
-                    "t", "u", "v", "w", "x", "y", "z"]
+       self.letter_list = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R",
+                       "S",
+                       "T", "U", "V", "W", "X", "Y", "Z"]
+       letter_list = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S",
+                  "T", "U", "V", "W", "X", "Y", "Z"]
+       lowerr_list = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s",
+                  "t", "u", "v", "w", "x", "y", "z"]
 
-     self.letter_dict = {}
+       self.letter_dict = {}
 
-     for pos in range(len(letter_list)):
-         self.letter_dict[letter_list[pos]] = lowerr_list[pos]
-     line_list = []
-     self.actual_answer = []
-     story = ""
-     for line in self.text_file:
-         for char in line:
-             if char != " " and char != "\n":
-                 self.actual_answer.append(char)
-         self.char_per_line = 0
-         for char in line:
-             if char == ".":
-                 random_char = random.choice(letter_list)
-                 line_list.append(random_char)
-                 self.char_per_line += 1
-             elif char == " ":
-                 line_list.append(char)
-             else:
-                 line_list.append(char)
-                 self.char_per_line += 1
-         for char in line_list:
-             story += char
-         line_list = []
-     self.story_txt = Text(self, width=45, height=17, wrap=WORD, font="Courier 18", bg="light salmon")
-     self.story_txt.grid(row=0, column=0, columnspan=4)
-     self.story_txt.insert(0.0, story)
+       for pos in range(len(letter_list)):
+           self.letter_dict[letter_list[pos]] = lowerr_list[pos]
+       line_list = []
+       self.actual_answer = []
+       story = ""
+       for line in self.text_file:
+           for char in line:
+               if char != " " and char != "\n":
+                   self.actual_answer.append(char)
+           self.char_per_line = 0
+           for char in line:
+               if char == ".":
+                   random_char = random.choice(letter_list)
+                   line_list.append(random_char)
+                   self.char_per_line += 1
+               elif char == " ":
+                   line_list.append(char)
+               else:
+                   line_list.append(char)
+                   self.char_per_line += 1
+           for char in line_list:
+               story += char
+           line_list = []
+       self.story_txt = Text(self, width=46, height=17, wrap=WORD, font="Courier 18", bg="light salmon")
+       self.story_txt.grid(row=0, column=0, columnspan=4)
+       self.story_txt.insert(0.0, story)
 
-     Button(self, text="Check", bg="tomato", fg="black",
-            font="Courier 15 bold", bd=5, command=self.check
-            ).grid(row=9, column=0, sticky=W)
+       Label(self, text="", bg="coral", font="Courier 12"
+             ).grid(row=16, column=0, sticky=W)
+       Button(self, text="Check", bg="tomato", fg="black",
+              font="Courier 15 bold", bd=5, command=self.check
+              ).grid(row=17, column=1, sticky=E)
+       Button(self, text="Quit", bg="tomato", fg="black",
+              font="Courier 15 bold", bd=5, command=self.quit
+              ).grid(row=17, column=2, sticky=W)
 
+       if self.actual_answer[2] == "P":
+           self.word_list = ["phoenix", "prophecy", "evil", "thestral", "darkness"]
+       if self.actual_answer[17] == "M":
+           self.word_list = ["avox", "bow and arrow", "district twelve", "forcefield", "hunger games", "jabberjay",
+                             "mockingjay", "muttation", "nightlock berry", "panem"]
+           self.char_per_line -= 1
+       if self.actual_answer[31] == "O":
+           self.word_list = ["ashes", "inferno", "phoenix", "blaze", "ignite", "burn", "flame", "kindle", "spark",
+                             "smolder", "smoke", "combust", "pyrokinetic", "incandescent", "heat"]
 
-     if self.actual_answer[2] == "P":
-         self.word_list = ["phoenix", "prophecy", "evil", "thestral", "darkness"]
-     if self.actual_answer[17] == "M":
-         self.word_list = ["avox", "bow and arrow", "district twelve", "forcefield", "hunger games", "jabberjay",
-                           "mockingjay", "muttation", "nightlock berry", "panem"]
-     if self.actual_answer[31] == "O":
-         self.word_list = ["ashes", "inferno", "phoenix", "blaze", "ignite", "burn", "flame", "kindle", "spark",
-                           "smolder", "smoke", "combust", "pyrokinetic", "incandescent", "heat"]
+       Label(self, text=" ", bg="coral").grid(row=0, column=50, sticky=E)
+       row_count = 2
+       column_count = 0
+       for word in self.word_list:
+           if column_count == 5:
+               row_count += 1
+               column_count = 0
+           Label(self, text=word, bg="coral", font="Courier 12"
+                 ).grid(row=row_count, column=column_count, sticky=W)
+           column_count += 1
 
-     Label(self, text=" ", bg="coral").grid(row=0, column=50, sticky=E)
-     row_count = 2
-     column_count = 0
-     for word in self.word_list:
-         if column_count == 5:
-             row_count += 1
-             column_count = 0
-         Label(self, text=word, bg="coral", font="Courier 12"
-               ).grid(row=row_count, column=column_count, sticky=W)
-         column_count += 1
+   def check(self):
+       story = ""
 
- def check(self):
-     story = ""
+       answer_list = []
+       answers = self.story_txt.get(0.0, END)
+       for thing in answers:
+           if thing != " " and thing != "\n":
+               answer_list.append(thing)
+       char_count = 0
+       for pos in range(len(answer_list)):
+           if char_count == self.char_per_line:
+               story += "\n"
+               char_count = 0
+           if answer_list[pos] == "*":
+               if self.actual_answer[pos] != ".":
+                   story += (self.letter_dict[self.actual_answer[pos]])
+                   story += " "
+                   char_count += 1
 
-     answer_list = []
-     answers = self.story_txt.get(0.0, END)
-     for thing in answers:
-         if thing != " " and thing != "\n":
-             answer_list.append(thing)
-     char_count = 0
-     for pos in range(len(answer_list)):
-         if char_count == self.char_per_line:
-             story += "\n"
-             char_count = 0
-         if answer_list[pos] == "*":
-             if self.actual_answer[pos] != ".":
-                 story += (self.letter_dict[self.actual_answer[pos]])
-                 story += " "
-                 char_count += 1
+               else:
+                   story += random.choice(self.letter_list)
+                   story += " "
+                   char_count += 1
 
-             else:
-                 story += random.choice(self.letter_list)
-                 story += " "
-                 char_count += 1
+           else:
+               story += answer_list[pos]
+               story += " "
+               char_count += 1
+       self.story_txt.delete(0.0, END)
+       self.story_txt.insert(0.0, story)
 
-         else:
-             story += answer_list[pos]
-             story += " "
-             char_count += 1
-
-     self.story_txt.delete(0.0, END)
-     self.story_txt.insert(0.0, story)
+   def quit(self):
+       self.end_screen()
